da93c5b644876949718ca62bd654bf977298e765
Mario Vercellotti <mario.vercellotti@wunder.io>
2021-08-27 15:51:44 +0300

    Add the individual post page.

--- components/Article.js
+++ components/Article.js
@@ -29,3 +29,34 @@ export function ArticleTeaser({ article }) {
     </div>
   );
 }
+
+export function ArticleFull({ article }) {
+  return (
+    <article className="article-full">
+      <h2>{article.title}</h2>
+      <small>
+        By <strong>{article.uid.display_name}</strong>
+      </small>
+
+      {article.field_image?.uri && (
+        <figure>
+          <Image
+            src={`${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}${article.field_image.uri.url}`}
+            width={768}
+            height={400}
+            layout="responsive"
+            objectFit="cover"
+            alt={article.field_image.resourceIdObjMeta.alt}
+          />
+        </figure>
+      )}
+
+      {article.body?.processed && (
+        <div
+          className="article-body"
+          dangerouslySetInnerHTML={{ __html: article.body?.processed }}
+        />
+      )}
+    </article>
+  );
+}
